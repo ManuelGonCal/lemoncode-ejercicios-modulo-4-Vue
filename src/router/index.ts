7f07a434e303c3102981e91d20bd58477d20d012
Manuel Gonzalez <manuelmaingonzalez@gus.global>
2021-02-26 15:38:21 +0100

    vuetify added to project

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,19 +8,19 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
     name: "About",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+  mode: "history",
+  routes,
 });
 
 export default router;
