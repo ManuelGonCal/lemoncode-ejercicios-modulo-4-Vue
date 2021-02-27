59e1ce3273f97ac706ea987acd63106218699a4a
Manuel Gonzalez <manuelmaingonzalez@gus.global>
2021-02-27 19:29:29 +0100

    MembersList updated to show the members

---
+++ todoList.js
@@ -0,0 +1,2 @@
+// TODO: conseguir el value del input en el formulario de MembersList y ser capaz de buscar por el value y volver a mostrar la lista,
+// TODO Adicionalmente ser capaz de conseguir externalizar el formulario a otro componente que sea el que le pasa la lista a pintar al componente de Lista
